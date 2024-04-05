8eebfe71547d69ace2a3a97834ff43073dbeb2af
AndresMosquera9005 <andrubjj@gmail.com>
2024-04-05 14:43:00 -0500

    1 modificacion en css

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,5 +1,39 @@
-let boton_login = document.getElementById("boton-login");
+document.addEventListener("DOMContentLoaded", function(event) {
+    let botonLogin = document.getElementById("botonLogin");
+    if (botonLogin) {
+        botonLogin.addEventListener("click", () => {
+        alert("Espere un momento por favor ...");
+    });
+    } 
 
-boton_login.addEventListener("click", () => { 
-    alert("espere un momento por favor ...")
-})
+    let botonEnviar = document.getElementById("btn-enviar");
+    if (botonEnviar) {
+        botonEnviar.addEventListener("click", () => {
+        alert("El formulario fue enviado exitosamente");
+    });
+    }
+
+});
+
+
+// let boton_login = document.getElementById("botonLogin");
+//let btn_enviar = document.getElementById("btn-enviar");
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     let botonLogin = document.getElementById("botonLogin");
+//     if (botonLogin) {
+//         botonLogin.addEventListener("click", () => {
+//         alert("Espere un momento por favor ...");
+//     });
+//     } 
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     let botonEnviar = document.getElementById("btn-enviar");
+//     if (botonEnviar) {
+//         botonEnviar.addEventListener("click", () => {
+//         alert("El formulario fue enviado exitosamente");
+//     });
+//     } 
+// });
+
